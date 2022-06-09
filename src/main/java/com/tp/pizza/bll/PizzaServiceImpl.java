1f272898be25b2de05a6a82a6bbb21ffa9ef3d72
Neytis7 <curtis.meda2021@campus-eni.fr>
2022-06-09 16:12:29 +0200

    Gestion profile

--- src/main/java/com/tp/pizza/bll/PizzaServiceImpl.java
+++ src/main/java/com/tp/pizza/bll/PizzaServiceImpl.java
@@ -3,10 +3,12 @@ package com.tp.pizza.bll;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import com.tp.pizza.bo.Pizza;
 
 @Service
+@Profile("test")
 public class PizzaServiceImpl implements PizzaService {
 
   List<Pizza> listPizza;
--- src/main/java/com/tp/pizza/bll/ServicePizzaProd.java
+++ src/main/java/com/tp/pizza/bll/ServicePizzaProd.java
@@ -1,10 +1,8 @@
 package com.tp.pizza.bll;
 
 import java.util.List;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
 import com.tp.pizza.bo.Pizza;
 import com.tp.pizza.dal.PizzaRepository;
 
@@ -12,37 +10,36 @@ import com.tp.pizza.dal.PizzaRepository;
 @Profile("Default")
 public class ServicePizzaProd implements PizzaService {
 
-	private PizzaRepository pizzaRepository;
-	
-	public ServicePizzaProd(PizzaRepository pizzaRepository)
-	{
-		this.pizzaRepository = pizzaRepository;
-	}
-	
-	@Override
-	public List<Pizza> getPizzas() {
-		return this.pizzaRepository.findAll();
-	}
+  private PizzaRepository pizzaRepository;
 
-	@Override
-	public Pizza getPizzaById(int id) {
-		return this.pizzaRepository.findById(id);
-	}
+  public ServicePizzaProd(PizzaRepository pizzaRepository) {
+    this.pizzaRepository = pizzaRepository;
+  }
 
-	@Override
-	public void addPizza(Pizza pizza) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public List<Pizza> getPizzas() {
+    return this.pizzaRepository.findAll();
+  }
 
-	@Override
-	public void modifiedPizza(Pizza pizza) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public Pizza getPizzaById(int id) {
+    return this.pizzaRepository.findById(id);
+  }
 
-	@Override
-	public void deletePizza(Pizza pizza) {
-		this.pizzaRepository.delete(pizza);
-	}
+  @Override
+  public void addPizza(Pizza pizza) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void modifiedPizza(Pizza pizza) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void deletePizza(Pizza pizza) {
+    this.pizzaRepository.delete(pizza);
+  }
 }
