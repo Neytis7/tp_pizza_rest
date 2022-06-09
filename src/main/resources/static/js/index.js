3ffb8742d7a8669aba9641bd76a443eb6d5e9fec
theofaf <theophane.fafet2021@campus-eni.fr>
2022-06-09 18:28:00 +0200

    mise en forme liste pizzas

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,17 +4,33 @@ function chargement(){
   .then(createList)
 }
 
-const listePizza = document.getElementById('listPizza')
+const listePizza = document.getElementById('listPizza');
+
+const btnAjout = document.getElementById("btnAjoutPizza");
+
 function createList(pizzas){
 	listePizza.innerHTML= ""
 	pizzas.forEach(function (pizza) {
 		listePizza.innerHTML += `
-		<div class="card" data-id=${pizza.id}>
-			<h2>${pizza.nom}</h2>
-			<img src="/img/${pizza.image}"/>
-		</div>
-		`
-		})
+		<div class="card mb-12">
+		  <h3 class="card-header text-center">${pizza.nom}</h3>
+		  
+		  <div class="card-body">
+		    <p class="card-text text-center">${pizza.description}</p>
+		  </div>
+		  
+		  <img width="300" src="/img/${pizza.image}" style="position:relative;left:400px;" alt="une pizza"/>
+		  
+		  <div class="card-footer text-muted text-center">
+		    ${pizza.prix} €
+		  </div>
+		</div>`
+	})
 }
 
 chargement();
+
+
+btnAjout.addEventListener('click', event => {
+  listePizza.style.display = "none";
+});
