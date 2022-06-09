7a63ef7f3442a82cb8e3725e542bba95079ae96b
Neytis7 <curtis.meda2021@campus-eni.fr>
2022-06-09 11:26:48 +0200

    First commit

---
+++ src/main/java/com/tp/pizza/TpPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.tp.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/pizza/TpPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
